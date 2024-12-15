268950bed921c20f4f974669d3102da635645041
heelyash <heelyash78@gmail.com>
2024-12-15 10:03:03 +0530

    Add files via upload

---
+++ Yash SQL ASS.sql
@@ -0,0 +1,26 @@
+create database jungle;
+use world;
+use canada;
+select count(tip) from  taxis;
+select  count(tip) from tips;
+
+create database new_database;
+use new_database;
+use mavenmovies;
+select* from actor;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
